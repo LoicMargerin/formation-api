129e210cc93a51579fca0a20f1f2820a68c681d8
D8484 <loic.margerin@arkea.com>
2024-12-16 16:00:11 +0100

    DAO DB

---
+++ src/main/java/org/enib/renew/dao/db/impl/DevisesDAODbImpl.java
@@ -0,0 +1,77 @@
+package org.enib.renew.dao.db.impl;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.enib.renew.dao.IDevisesDAO;
+import org.enib.renew.dao.db.AbstractDBDAO;
+import org.enib.renew.dto.DeviseDTO;
+import org.enib.renew.exceptions.DAOException;
+import org.springframework.context.annotation.Primary;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.List;
+
+@Repository
+@Primary
+public class DevisesDAODbImpl extends AbstractDBDAO implements IDevisesDAO {
+
+    private static String REQUETE_GET_ALL_DEVISES = " SELECT code, libelle FROM devises";
+    private static String REQUETE_GET_DEVISE = " SELECT code, libelle FROM devises WHERE code = :code";
+
+    protected DevisesDAODbImpl(HikariDataSource dpDataSource) {
+        super(dpDataSource);
+    }
+
+    private final RowMapper<DeviseDTO> rowMapperDeviseDTO = (rs, rowNum) -> {
+        final DeviseDTO fromDB = new DeviseDTO();
+        fromDB.setCode(StringUtils.trim(rs.getString("code")));
+        fromDB.setLibelle(StringUtils.trim(rs.getString("libelle")));
+
+        return fromDB;
+    };
+
+    /**
+     * Récupérer une devise par son code
+     *
+     * @param pCodeDevise le code à requéter
+     * @return la devise si trouvée, null sinon
+     * @throws DAOException en cas de crash
+     */
+    @Override
+    public DeviseDTO getDeviseByCode(String pCodeDevise) throws DAOException {
+        final var parameters = new MapSqlParameterSource()
+                .addValue("code", pCodeDevise);
+
+        try {
+            final List<DeviseDTO> fromDb = super.getTemplate().query(REQUETE_GET_DEVISE, parameters, rowMapperDeviseDTO);
+            if (CollectionUtils.isNotEmpty(fromDb)) {
+                return fromDb.getFirst();
+            } else {
+                return null;
+            }
+
+        } catch (final DataAccessException pEx ) {
+            throw new DAOException(pEx);
+        }
+    }
+
+    /**
+     * Récupérer la liste des devises
+     *
+     * @return la liste des devises
+     */
+    @Override
+    public List<DeviseDTO> getDevises() throws DAOException {
+        try {
+            final var parameters = new MapSqlParameterSource();
+
+            return super.getTemplate().query(REQUETE_GET_ALL_DEVISES, parameters, rowMapperDeviseDTO);
+        } catch (final DataAccessException pEx ) {
+            throw new DAOException(pEx);
+        }
+    }
+}
---
+++ src/main/java/org/enib/renew/dao/db/impl/SoldesDAODbImpl.java
@@ -0,0 +1,79 @@
+package org.enib.renew.dao.db.impl;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.enib.renew.dao.ISoldesDAO;
+import org.enib.renew.dao.db.AbstractDBDAO;
+import org.enib.renew.dto.DeviseDTO;
+import org.enib.renew.dto.SoldeDTO;
+import org.enib.renew.exceptions.DAOException;
+import org.springframework.context.annotation.Primary;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Primary
+public class SoldesDAODbImpl extends AbstractDBDAO implements ISoldesDAO {
+
+    private static String REQUETE_GET_ALL_PERSONNES = " SELECT id FROM soldes";
+    private static String REQUETE_GET_SOLDE = " SELECT id, montant, devise FROM soldes WHERE id = :id";
+
+    protected SoldesDAODbImpl(HikariDataSource dpDataSource) {
+        super(dpDataSource);
+    }
+
+    private final RowMapper<SoldeDTO> rowSoldeDeviseDTO = (rs, rowNum) -> {
+        final SoldeDTO fromDB = new SoldeDTO();
+        fromDB.setPersonId(StringUtils.trim(rs.getString("id")));
+        fromDB.setSolde(rs.getDouble("montant"));
+        fromDB.setDeviseCode(StringUtils.trim(rs.getString("devise")));
+
+        return fromDB;
+    };
+
+    /**
+     * Récupérer une devise par son code
+     *
+     * @param pPersonId le code à requéter
+     * @return la devise si trouvée, null sinon
+     * @throws DAOException en cas de crash
+     */
+    @Override
+    public SoldeDTO getSolde(String pPersonId) throws DAOException {
+        final var parameters = new MapSqlParameterSource()
+                .addValue("id", pPersonId);
+
+        try {
+            final List<SoldeDTO> fromDb = super.getTemplate().query(REQUETE_GET_SOLDE, parameters, rowSoldeDeviseDTO);
+            if (CollectionUtils.isNotEmpty(fromDb)) {
+                return fromDb.getFirst();
+            } else {
+                return null;
+            }
+
+        } catch (final DataAccessException pEx ) {
+            throw new DAOException(pEx);
+        }
+    }
+
+    /**
+     * Récupérer la liste des devises
+     *
+     * @return la liste des devises
+     */
+    @Override
+    public List<String> getPersonIds() throws DAOException {
+        try {
+            final var parameters = new MapSqlParameterSource();
+
+            return CollectionUtils.emptyIfNull(super.getTemplate().query(REQUETE_GET_ALL_PERSONNES, parameters, rowSoldeDeviseDTO)).stream().map(SoldeDTO::getPersonId).toList();
+        } catch (final DataAccessException pEx ) {
+            throw new DAOException(pEx);
+        }
+    }
+}
