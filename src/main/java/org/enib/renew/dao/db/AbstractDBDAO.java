92c1b16cc859db262fa4b4bd69d627da5c65bcf3
D8484 <loic.margerin@arkea.com>
2024-12-16 16:00:11 +0100

    DAO DB

--- src/main/java/org/enib/renew/dao/db/AbstractDBDAO.java
+++ src/main/java/org/enib/renew/dao/db/AbstractDBDAO.java
@@ -2,14 +2,22 @@ package org.enib.renew.dao.db;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 /**
  * Superclasse des DAO PostgreSQL
  */
 public class AbstractDBDAO {
     protected final HikariDataSource dataSource;
+    protected final NamedParameterJdbcTemplate template;
 
     protected AbstractDBDAO(@Qualifier("DataBaseDatasource") HikariDataSource dpDataSource) {
         this.dataSource = dpDataSource;
+        this.template = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    protected NamedParameterJdbcTemplate getTemplate() {
+        return this.template;
     }
 }
--- src/main/java/org/enib/renew/exceptions/DAOException.java
+++ src/main/java/org/enib/renew/exceptions/DAOException.java
@@ -1,13 +1,19 @@
 package org.enib.renew.exceptions;
 
+import org.springframework.dao.DataAccessException;
+
 import java.io.IOException;
 
 public class DAOException extends Exception {
-    public DAOException(IOException pEx) {
+    public DAOException(Exception pEx) {
         super(pEx);
     }
 
     public DAOException(String pMessage) {
         super(pMessage);
+    }
+
+    public DAOException(String s, DataAccessException pEx) {
+        super(pEx);
     }
 }
